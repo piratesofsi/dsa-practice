fa983d3e640ffa3a1b70874e8b9fdc72c848939b
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2025-08-03 09:07:24 +0530

    intersection of two sorted arrays(optimal)
    it's an optimal approach with an tc of (n1+n2) and space complexity is o(1) called an extra space if it is asked what space is used it will be (n1+n2)

---
+++ 6.easy -arrays/intersectionofsortedarrayoptimal.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Myarray {
+
+	static ArrayList<Integer> intersection(int arr1[], int arr2[], int m, int n) {
+		int i = 0;
+		int j = 0;
+		ArrayList<Integer> a1 = new ArrayList<>();
+		while (i < m && j < n) {
+			if (arr1[i] == arr2[j]) {
+				a1.add(arr1[i]);
+				i++;
+				j++;
+
+			} else if (arr1[i] < arr2[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		return a1;
+	}
+
+
+
+
+	static void printarr(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(" " + arr[i]);
+		}
+	}
+}
+
+
+
+public class intersectionofsortedarrayoptimal {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the first  array : ");
+		int m = sc.nextInt();
+		System.out.println("Enter the size of the second  array : ");
+		int n = sc.nextInt();
+		int arr_1[] = new int[m];
+		int arr_2[] = new int[n];
+		System.out.println("Enter the " + m + "elements of the first array : ");
+		for (int i = 0; i < arr_1.length; i++) {
+			arr_1[i] = sc.nextInt();
+		}
+		System.out.println("Enter the " + n + "elements of the first array : ");
+		for (int i = 0; i < arr_2.length; i++) {
+			arr_2[i] = sc.nextInt();
+		}
+
+		System.out.println("first array entered by the user : ");
+		Myarray.printarr(arr_1);
+		System.out.println("second array entered by the user : ");
+		Myarray.printarr(arr_2);
+		System.out.println();
+		System.out.println("intersection array : ");
+		ArrayList<Integer> result = Myarray.intersection(arr_1, arr_2, m, n);
+
+		for (var i : result) {
+			System.out.print(" " + i);
+		}
+		sc.close();
+	}
+}
