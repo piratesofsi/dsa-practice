48ecf1a63c1d02feaac961fde55a61fda9e014a1
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2025-08-07 21:22:37 +0530

    element which occurs once and others twice
    element which occurs once and others twice most optimal approach by using xor

---
+++ 6.easy -arrays/nowhichoccursonesandothertwiceoptimalxor.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+class Myarray {
+
+	static int once(int arr[]) {
+		int xor = 0;
+		for (int i = 0; i < arr.length; i++) {
+			xor ^= arr[i];
+		}
+		return xor;
+	}
+
+	static void printarr(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(" " + arr[i]);
+		}
+	}
+}
+public class nowhichoccursonesandothertwiceoptimalxor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int n = sc.nextInt();
+		int arr_1[] = new int[n];
+		System.out.println("Enter the " + n + " elements of the array : ");
+		for (int i = 0; i < arr_1.length; i++) {
+			arr_1[i] = sc.nextInt();
+		}
+
+
+		System.out.println("array entered by the user : ");
+		Myarray.printarr(arr_1);
+
+		System.out.println();
+		int result = Myarray.once(arr_1);
+		System.out.println("the no which is occured only ones is : " + result);
+		sc.close();
+	}
+}
