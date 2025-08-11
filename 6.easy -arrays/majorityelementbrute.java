4c3f247085c6c8e615078dacbab2917acd77b088
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2025-08-11 20:06:16 +0530

    finding majority element >n/2 brute
    1. Brute Force Approach (Nested Loops)
    Description:
    
    For each element in the array, loop through the entire array again to count how many times it appears.
    
    If any element’s count is greater than n/2, that element is the majority.
    
    This is the simplest method, but inefficient for large arrays.
    
    Complexity:
    
    Time: O(n²) — two nested loops.
    
    Space: O(1) — no extra data structures.

---
+++ 6.easy -arrays/majorityelementbrute.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Myarray {
+	static int zadakimajority(int arr[], int n) {
+		for (int i = 0; i < arr.length; i++) {
+			int count = 0;
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+				}
+
+			} if (count > n / 2) {
+				return arr[i];
+			}
+		}
+		return -1;
+	}
+	static void printarr(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(" " + arr[i]);
+		}
+	}
+}
+public class majorityelementbrute {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int n = sc.nextInt();
+		int arr_1[] = new int[n];
+		System.out.println("Enter the " + n + " elements of the array : ");
+		for (int i = 0; i < arr_1.length; i++) {
+			arr_1[i] = sc.nextInt();
+		}
+		System.out.println("array entered by the user : ");
+		Myarray.printarr(arr_1);
+		System.out.println();
+		int result = Myarray.zadakimajority(arr_1, n);
+		System.out.println("majority element in the array : " + result);
+		sc.close();
+
+	}
+}
