de31cc339ccb69d5e7dd05a0a0299c8f619da798
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2025-08-29 17:58:13 +0530

    majority element better singe iteration
    2️⃣ HashMap / Frequency Count Approach
    
    Logic:
    
    Use a HashMap to store counts of each element.
    
    Iterate the map and pick elements with frequency > n/3.
    
    Time Complexity: O(n)
    Space Complexity: O(n)
    
    Notes:
    
    Very straightforward, avoids nested loops.
    
    Still uses extra space.

---
+++ 8.hard-arrays/majorityelement3betterhashmapless.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Myarray{
+
+	static ArrayList<Integer> zadakimajority(ArrayList<Integer> arr){
+		HashMap<Integer,Integer> map=new HashMap<>();
+		int n=arr.size();
+		int mm=(n/3)+1;
+		ArrayList<Integer> ans=new ArrayList<>();
+	   //taking the element and their count in the hashmap
+		for(int num:arr){
+			map.put(num,map.getOrDefault(num,0)+1);
+
+			if(map.get(num)==mm){
+				ans.add(num);
+			}
+		}
+
+
+		
+		Collections.sort(ans);
+		return ans;
+	}
+
+
+
+	static void printarr(ArrayList<Integer> arr){
+		for(int num:arr){
+			System.out.print(" "+num);
+		}
+	}
+}
+
+public class majorityelement3betterhashmapless{
+	
+		 public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();  // size of array
+
+        ArrayList<Integer> arr_1 = new ArrayList<>();
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr_1.add(sc.nextInt());  // add elements into ArrayList
+        }
+
+        System.out.println("The array entered by the user: ");
+        Myarray.printarr(arr_1);
+          
+        ArrayList<Integer> result=Myarray.zadakimajority(arr_1);
+        Myarray.printarr(result);
+        System.out.println();
+        System.out.println("the element which have the most majority in the array is : ");
+        Myarray.printarr(result);
+        sc.close();
+
+	}
+}
