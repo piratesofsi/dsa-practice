4cc83370bd94bd3087cf90f3361b867165d2d333
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2025-08-30 21:10:11 +0530

    4sum better(hashset) approach
    HashSet Optimization (3 Loops + 2-Sum HashSet)
    
    Logic:
    
    Fix first three numbers (i, j, k)
    
    Use HashSet to check if the required fourth number exists
    
    TC: O(n³) average — 3 loops + O(1) HashSet lookups
    
    SC: O(n³) — to store unique quadruplets
    
    Notes: Much faster than brute force; avoids one loop using HashSet.

---
+++ 8.hard-arrays/foursumbetter.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+
+class Myarray{
+	static ArrayList<ArrayList<Integer>> sum(ArrayList<Integer> arr,int target){
+		HashSet<ArrayList<Integer>> set=new HashSet<>();
+		ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+		for(int i=0;i<arr.size();i++){
+			
+			for(int j=i+1;j<arr.size();j++){
+				Set<Integer> set1=new HashSet<>();
+				for(int k=j+1;k<arr.size();k++){
+                   int p=-(arr.get(i)+arr.get(j)+arr.get(k))+target;
+                   if(set1.contains(p)){
+                   	ArrayList<Integer> temp=new ArrayList<>();
+                   	temp.add(arr.get(i));
+                   		temp.add(arr.get(j));
+                   			temp.add(arr.get(k));
+                   				temp.add(p);
+                         Collections.sort(temp);
+                         set.add(temp);
+
+
+                   }
+                   else {
+                   	set1.add(arr.get(k));
+                   }
+
+
+
+
+				}		
+             
+			}
+		}
+        ans.addAll(set);
+		return ans;
+
+	}
+
+	static void printarr(ArrayList<Integer> arr){
+		for(int num:arr){
+			System.out.print(" "+num);
+		}
+	}
+}
+public class foursumbetter{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array: ");
+		int n=sc.nextInt();
+		ArrayList<Integer> arr_1=new ArrayList<>();
+		System.out.println("Enter "+n+" elements of the array : ");
+		for(int i=0;i<n;i++){
+			arr_1.add(sc.nextInt());
+		}
+		System.out.println("Enter the target : ");
+		int k=sc.nextInt();
+
+		System.out.println("Array entered by the user : ");
+		Myarray.printarr(arr_1);
+		System.out.println();
+		ArrayList<ArrayList<Integer>> result=Myarray.sum(arr_1,k);
+		System.out.println("the triplets are : ");
+		if(result.isEmpty()){
+			System.out.println(" No triplet in the array sums to "+ k);
+		} else {
+		for(ArrayList<Integer> num:result){
+			System.out.println(result);
+		}
+	}
+		sc.close();
+
+	}
+
+}
