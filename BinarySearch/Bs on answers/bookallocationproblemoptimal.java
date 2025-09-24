abdd87986d535a82365543b31206ec6c869db62d
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2025-09-24 20:59:28 +0530

    Add files via upload

---
+++ BinarySearch/Bs on answers/bookallocationproblemoptimal.java
@@ -0,0 +1,95 @@
+import java.util.*;
+class MyArray{
+
+
+	//for calculating summation 
+	static int summation(int arr[]){
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
+			sum+=arr[i];
+		}
+		return sum;
+	}
+
+
+
+	//for calculating maximum 
+	static int maximum(int arr[]){
+		int greater=Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++){
+			greater=Math.max(greater,arr[i]);
+		}
+		return greater;
+	}
+
+	static int noofstudent(int arr[],int pages){
+	 //if the students are more and books are leess 
+		//it can be solved as the it breaks the 
+		//basic rule of the problem 
+		//each student should atleast have 1 book 
+		if(students>arr.length)
+			return -1;
+		int student=1,countofstudent=0;
+		for(int i=0;i<arr.length;i++){
+			//we the next arr[i] can be added 
+			if((countofstudent+arr[i])<=pages){
+				countofstudent+=arr[i];
+			}
+
+			else{
+				student++;
+				countofstudent=arr[i];
+			}
+
+
+		}
+		return student;
+	}
+
+	static int bookall(int arr[],int students){
+	   int low = maximum(arr), high=summation(arr);
+
+	   while(low<=high){
+	   	int mid=low+(high-low)/2;
+	   	if(noofstudent(arr,mid)<=students)
+	   		high=mid-1;
+	   	else
+	   		low=mid+1;
+	   }
+
+	   return low;
+	}
+
+
+
+	static void printArr(int arr[]){
+		for(int i=0;i<arr.length;i++){
+			System.out.print(" "+arr[i]);
+		}
+	}
+}
+public class bookallocationproblemoptimal{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int n=sc.nextInt();
+		int arr_1[]=new int[n];
+		System.out.println("Enter the "+n+" Elements of the array: ");
+		for(int i=0;i<arr_1.length;i++){
+			arr_1[i]=sc.nextInt();
+		}
+
+		System.out.println("Enter the no of students : ");
+		int k=sc.nextInt();
+		System.out.println("Array entered by the user : ");
+		MyArray.printArr(arr_1);
+		System.out.println();
+
+		int result=MyArray.bookall(arr_1,k);
+		if(result==-1)
+			System.out.println("solution doesn't exists!!!");
+		else 
+			System.out.println("the maximum allocation after minimising : "+ result);
+		sc.close();
+	}
+}
